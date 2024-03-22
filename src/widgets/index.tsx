ab422d8ea4d9e18ca85d56c03ec622ff3daf9b32
Valentin Panchenko <senior.konung@gmail.com>
2024-03-22 15:46:52 +0300

    fix: remove navigation registration

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -4,7 +4,7 @@ import _ from 'lodash';
 import '../styles/style.css';
 
 const NAVIGATION_POWERUP = 'navigation_powerup';
-const TOTALS_POWERUP = 'totals_powerup'
+const TOTALS_POWERUP = 'totals_powerup';
 
 const EventQueueCompleteCard = _.once((plugin: SDK.RNPlugin) => {
     return {
@@ -13,7 +13,7 @@ const EventQueueCompleteCard = _.once((plugin: SDK.RNPlugin) => {
         handler: function () {
             App.incrementRitualFlashcards(plugin);
         },
-    }
+    };
 });
 
 async function onActivate(plugin: SDK.ReactRNPlugin) {
@@ -22,61 +22,45 @@ async function onActivate(plugin: SDK.ReactRNPlugin) {
         name: 'Итоги',
         description: '',
         options: {
-            properties: []
-        }
-    })
+            properties: [],
+        },
+    });
 
-    const totalsPowerup = await plugin.powerup.getPowerupByCode(TOTALS_POWERUP)
-    if (_.isUndefined(totalsPowerup)) return
+    const totalsPowerup = await plugin.powerup.getPowerupByCode(TOTALS_POWERUP);
+    if (_.isUndefined(totalsPowerup)) return;
 
     const mainTotalsRem = await _.block(async () => {
-        const children = await totalsPowerup?.getChildrenRem()
-        if (_.isNotUndefined(children) && children.length > 0) return children[0]
-        
-        const mainRem = await plugin.rem.createRem()
-        if (_.isUndefined(mainRem)) return
-        
-        await mainRem.setParent(totalsPowerup ?? null)
-        return mainRem
-    })
-    if (_.isUndefined(mainTotalsRem)) return
+        const children = await totalsPowerup?.getChildrenRem();
+        if (_.isNotUndefined(children) && children.length > 0) return children[0];
 
+        const mainRem = await plugin.rem.createRem();
+        if (_.isUndefined(mainRem)) return;
+
+        await mainRem.setParent(totalsPowerup ?? null);
+        return mainRem;
+    });
+    if (_.isUndefined(mainTotalsRem)) return;
 
     await plugin.app.registerWidget('totals', SDK.WidgetLocation.UnderRemEditor, {
         remIdFilter: mainTotalsRem._id,
     });
 
-    await plugin.app.registerCSS('Styles for powerup totals', `
+    await plugin.app.registerCSS(
+        'Styles for powerup totals',
+        `
         div[data-document-id="${totalsPowerup._id}"] .rn-add-rem-button { display: none }
         div[data-node-id="${mainTotalsRem._id}"], div[data-children-node-id="${mainTotalsRem._id}"] { display: none; }
         div[data-document-id="${totalsPowerup._id}"] #document { max-width: 100% }
         div[data-rem-container-id="${mainTotalsRem._id}"] div:has(> .rn-plugin-root) { height: 80vh; padding-top: 1.5rem }
         div[data-children-node-id="${totalsPowerup._id}"] > div:nth-last-child(2) { display: none }
-    `)
+    `
+    );
 
     await plugin.app.registerSidebarButton({
         id: 'open_totals_widget',
         name: 'Итоги',
         async action() {
-            await totalsPowerup.openRemAsPage()
-        },
-    });
-
-    await plugin.app.registerPowerup({
-        name: 'Навигация',
-        code: NAVIGATION_POWERUP,
-        description: '',
-        options: {
-            properties: [],
-        },
-    });
-
-    await plugin.app.registerSidebarButton({
-        id: 'open_navigation_page',
-        name: 'Навигация',
-        async action() {
-            const navigationRem = await plugin.powerup.getPowerupByCode(NAVIGATION_POWERUP);
-            await navigationRem?.openRemAsPage();
+            await totalsPowerup.openRemAsPage();
         },
     });
 
@@ -85,37 +69,37 @@ async function onActivate(plugin: SDK.ReactRNPlugin) {
         name: 'Удалить пустые дочерние свойства',
         quickCode: 'rmpr',
         action: async () => {
-            const focusedRem = await plugin.focus.getFocusedRem()
-            if (_.isUndefined(focusedRem)) return
-            await App.removeEmptyChildProperties(plugin, focusedRem)
-        }
-    })
+            const focusedRem = await plugin.focus.getFocusedRem();
+            if (_.isUndefined(focusedRem)) return;
+            await App.removeEmptyChildProperties(plugin, focusedRem);
+        },
+    });
 
     await plugin.app.registerCommand({
         id: 'calculate_and_set_quota',
         name: 'Вычислить и установить квоту',
         quickCode: 'quota',
         action: async () => {
-            const focusedRem = await plugin.focus.getFocusedRem()
-            if (_.isUndefined(focusedRem)) return
-            const dailyRem = await App.findDailyDocInAncestors(focusedRem)
-            if (_.isUndefined(dailyRem)) return
-            await App.calculateAndSetQuota(plugin, dailyRem)
-        }
-    })
+            const focusedRem = await plugin.focus.getFocusedRem();
+            if (_.isUndefined(focusedRem)) return;
+            const dailyRem = await App.findDailyDocInAncestors(focusedRem);
+            if (_.isUndefined(dailyRem)) return;
+            await App.calculateAndSetQuota(plugin, dailyRem);
+        },
+    });
 
     await plugin.app.registerCommand({
         id: 'add_portal_product',
         name: 'Добавить портал на продукты',
         quickCode: 'addp',
         action: async () => {
-            const focusedRem = await plugin.focus.getFocusedRem()
-            if (_.isUndefined(focusedRem)) return
-            const dailyRem = await App.findDailyDocInAncestors(focusedRem)
-            if (_.isUndefined(dailyRem)) return
-            await App.addPortalProduct(plugin, dailyRem)
-        }
-    })
+            const focusedRem = await plugin.focus.getFocusedRem();
+            if (_.isUndefined(focusedRem)) return;
+            const dailyRem = await App.findDailyDocInAncestors(focusedRem);
+            if (_.isUndefined(dailyRem)) return;
+            await App.addPortalProduct(plugin, dailyRem);
+        },
+    });
 
     plugin.event.addListener(
         EventQueueCompleteCard(plugin).eventId,
